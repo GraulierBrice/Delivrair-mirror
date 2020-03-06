36f52006bde5442d70d5a00628536040baa7c55f
Valentin Campello <valentin.campello@etu.univ-cotedazur.fr>
2020-03-06 09:51:49 +0100

    #1 all component

---
+++ j2e/src/droneManagment/src/main/java/fr/polytech/team/n/App.java
@@ -0,0 +1,13 @@
+package fr.polytech.team.n;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
