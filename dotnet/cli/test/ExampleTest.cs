e9c26792757d5e208a949aad435e2be044fb9470
Théo Fafet <tfafet@gmail.com>
2020-03-13 15:37:20 +0100

    #3 add .csproj and dependencies to run tests

---
+++ dotnet/cli/test/ExampleTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using CLI;
+
+namespace Test
+{
+    public class ExampleTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
