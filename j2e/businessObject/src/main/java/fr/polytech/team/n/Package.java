2336c11363fe67614a779a62ea35cf506cbe67e1
GraulierBrice <brice.graulier@gmail.com>
2020-03-27 11:42:33 +0100

    #4 Set basic persistance
    Added Entity tag
    GeneratedValue Id (set to Longs)
    Empty constructor
    Setters and getters
    equals, hashcode and toString

--- j2e/businessObject/src/main/java/fr/polytech/team/n/Package.java
+++ j2e/businessObject/src/main/java/fr/polytech/team/n/Package.java
@@ -1,13 +1,70 @@
 package fr.polytech.team.n;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Package {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String address;
     private Customer customer;
 
-    public Package(int id, String address, Customer customer) {
+    public Package(){}
+
+    public Package(Long id, String address, Customer customer) {
         this.id = id;
         this.address = address;
         this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Package aPackage = (Package) o;
+        return getId() == aPackage.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Package{" +
+                "address='" + address + '\'' +
+                ", customer=" + customer +
+                '}';
     }
 }
