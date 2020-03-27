d6647815fc49247a1dcc527de7052cc01403da98
GraulierBrice <brice.graulier@gmail.com>
2020-03-27 12:31:35 +0100

    Revert "Revert "#4 Set basic persistance""
    This reverts commit 5aa300ed5bcf0118f6ffaf1e8e3c9034b32f8a2b.

--- j2e/businessObject/src/main/java/fr/polytech/team/n/Delivery.java
+++ j2e/businessObject/src/main/java/fr/polytech/team/n/Delivery.java
@@ -1,14 +1,60 @@
 package fr.polytech.team.n;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Calendar;
+import java.util.Objects;
 
+@Entity
 public class Delivery {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private Calendar date;
 
+    public Delivery(){}
 
-    public Delivery(int id, Calendar date) {
+    public Delivery(Long id, Calendar date) {
         this.id = id;
         this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Delivery delivery = (Delivery) o;
+        return getId() == delivery.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Delivery{" +
+                "date=" + date +
+                '}';
     }
 }
